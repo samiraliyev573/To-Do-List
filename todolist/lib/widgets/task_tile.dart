ca8bca2336fccd7c0ccbff37a615613f5ebce800
samiraliyev573 <63018640+samiraliyev573@users.noreply.github.com>
2020-12-30 00:36:25 -0800

    CheckBox functionality
    Checkbox functionality fixed using callbacks and lifitng the state up

--- todolist/lib/widgets/task_tile.dart
+++ todolist/lib/widgets/task_tile.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkBoxCallback(bool currentCheckBoxState) {
+    setState(() {
+      isChecked = currentCheckBoxState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text("This is a task."),
-      trailing: Checkbox(
-        value: false,
+      title: Text(
+        "This is a task.",
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckBox(
+          checkBoxState: isChecked, toggleCheckBoxState: checkBoxCallback),
     );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function toggleCheckBoxState;
+  TaskCheckBox({this.checkBoxState, this.toggleCheckBoxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(value: checkBoxState, onChanged: toggleCheckBoxState);
   }
 }
